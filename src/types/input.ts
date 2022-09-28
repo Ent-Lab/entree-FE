3b20be71f76daa40d3ab926e2cb417c585c420fe
task11 <task11@naver.com>
2022-09-28 19:50:36 +0900

    feat: Input component 타입 추가

---
+++ src/types/input.ts
@@ -0,0 +1,15 @@
+import { CSSProp } from "styled-components";
+
+export interface InputProps {
+  label: string;
+  value?: string;
+  type?: string;
+  size: 'sm' | 'md' | 'lg' | 'xlg';
+  placeholder: string;
+  disabled: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface StyledInputProps {
+  sizeStyle: CSSProp;
+}
