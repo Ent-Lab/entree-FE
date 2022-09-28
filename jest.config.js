b474f32e899a59993fe02028ff2b6000a4bba108
task11 <task11@naver.com>
2022-09-28 16:05:41 +0900

    EL-47 <chore: Set Jest config, setup & eslint config>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  preset: 'ts-jest'
-}
+  testPathIgnorePatterns: [ '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  'setupFilesAfterEnv': [
+    '<rootDir>/jest.setup.js'
+  ],
+  testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
