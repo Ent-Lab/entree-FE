e87ded555958a4ed733afb900bab1774245e6d7f
task11 <task11@naver.com>
2022-09-28 20:07:11 +0900

    refactor: input 테스트코드 리팩토링

--- src/__test__/Input.test.tsx
+++ src/__test__/Input.test.tsx
@@ -3,13 +3,12 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import Input from "@/components/Input/Input";
 
 describe('Input', () => {
-  it('Show Input placeholder', () => {
+  it('Show Input value', () => {
     const onChangeHandler = jest.fn();
 
-    render(
+    const { container } = render(
       <Input
         label={'test'}
-        value={'테스트'}
         size={'sm'}
         type='text'
         placeholder='테스트'
@@ -18,9 +17,8 @@ describe('Input', () => {
       />
     );
 
-    const input = screen.getByLabelText('test') as HTMLInputElement;
+    expect(container).toHaveTextContent('test');
 
-    expect(input.value).toBe('테스트');
   });
 
   context('When put a value in Input', () => {
