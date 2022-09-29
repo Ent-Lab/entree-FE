9a541fbeda7b84d1c15a77c1445ecd77541437a5
task11 <task11@naver.com>
2022-09-29 13:43:20 +0900

    feat: Header 레이아웃 구현

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,5 +1,14 @@
-import styled from "styled-components";
 import entreeLightLogo from "@/assets/images/entree-symbol-logo-dark.png";
+import defaultProfile from "@/assets/images/defaultProfile.jpeg";
+import { MdBookmark } from "@react-icons/all-files/md/MdBookmark";
+import { CgHome } from "@react-icons/all-files/cg/CgHome";
+import { CgDarkMode } from "@react-icons/all-files/cg/CgDarkMode";
+
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Avatar from "@/components/Avatar/Avatar";
+
 
 export default function Header() {
   return (
@@ -8,6 +17,10 @@ export default function Header() {
         <StyledLogoImg src={entreeLightLogo} />
       </StyledLogoWrapper>
       <StyledNavWrapper>
+        <CgDarkMode size={'35px'} />
+        <CgHome size={'35px'} />
+        <MdBookmark size={'35px'} />
+        <Avatar src={defaultProfile} size={'md'} />
       </StyledNavWrapper>
     </StyledHeader>
   );
@@ -17,6 +30,9 @@ const StyledHeader = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: black;
 `;
 
 const StyledLogoImg = styled.img`
@@ -25,9 +41,11 @@ const StyledLogoImg = styled.img`
 `;
 
 const StyledLogoWrapper = styled.div`
-  padding: 15px;
 `;
 
 const StyledNavWrapper = styled.div`
-  
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 15%;
 `;
