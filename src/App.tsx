5794da17ae249725386c0e2c38da90d69d10e589
task11 <task11@naver.com>
2022-10-07 16:16:51 +0900

    feat: theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import 'sanitize.css';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme, GlobalStyle } from '@/style';
 
+import { useRecoilState } from 'recoil';
+
+import { themeAtom } from './store/mode';
+
 import AppRouter from '@/routes/router';
+import { useLayoutEffect } from 'react';
 
 const App = () => {
+  const [theme, setTheme] = useRecoilState(themeAtom);
+
+  useLayoutEffect(() => {
+    const newTheme = localStorage.getItem('theme');
+    if (newTheme) {
+      setTheme(newTheme);
+    } else {
+      localStorage.setItem('theme', theme);
+    }
+  }, []);
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
       <GlobalStyle />
       <AppRouter />
     </ThemeProvider>
