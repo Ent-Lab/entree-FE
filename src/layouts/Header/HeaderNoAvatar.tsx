02fa7d80932f6ea280f8090f0015763859be3ddd
task11 <task11@naver.com>
2022-10-09 16:49:02 +0900

    feat: Modal 렌더링 구현

--- src/layouts/Header/HeaderNoAvatar.tsx
+++ src/layouts/Header/HeaderNoAvatar.tsx
@@ -1,8 +1,24 @@
+import ReactPortal from "@/components/Modal/ReactPortal";
+import { useToggleModal } from "@/utils/hooks";
 import styled from "styled-components";
 
 export default function HeaderNoAvatar() {
+  const { modalState, toggleModal } = useToggleModal();
+
   return (
-    <StyledButton type="button">로그인</StyledButton>
+    <>
+      <StyledButton
+        type="button"
+        onClick={toggleModal}
+      >
+        로그인
+      </StyledButton>
+      {modalState &&
+        <ReactPortal>
+          <div>로그인창</div>
+        </ReactPortal>
+      }
+    </>
   );
 };
 
