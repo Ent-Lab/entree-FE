58efa28b46c448cfb593dcb234afefea073604ad
task11 <task11@naver.com>
2022-10-09 16:50:11 +0900

    fix: Router 명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useLayoutEffect } from 'react';
 
 import { themeAtom } from './store/mode';
 
-import AppRouter from '@/routes/Router';
+import AppRouter from './routes/Router';
 
 const App = () => {
   const [theme, setTheme] = useRecoilState(themeAtom);
