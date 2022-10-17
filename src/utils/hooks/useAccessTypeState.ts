95974155abeb9afb590f63b3bb7ed55cf39d1a6f
task11 <task11@naver.com>
2022-10-17 14:58:13 +0900

    feat: useAccessType hook 구현

---
+++ src/utils/hooks/useAccessTypeState.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import { ACCESS_TYPE } from "../../constants";
+
+export const useAccessTypeState = () => {
+  const [accessType, setAccessType] = useState(ACCESS_TYPE.LANDING);
+
+  const handleAccessType = (accessType: string) => {
+    return accessType === ACCESS_TYPE.LOGIN ? ACCESS_TYPE.REGISTER : ACCESS_TYPE.LOGIN;
+  };
+
+  const changeAccessType = (accessType: string) => {
+    setAccessType(handleAccessType(accessType));
+  };
+
+  return { accessType, handleAccessType, changeAccessType };
+};
