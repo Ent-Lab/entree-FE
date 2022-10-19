d37fcaf01f90b77d7ccbcfe7f7549c5062032aab
task11 <task11@naver.com>
2022-10-20 01:11:56 +0900

    EL-72 <style: Loadmap layout 구현>

--- src/pages/HomePage/HomePage.style.tsx
+++ src/pages/HomePage/HomePage.style.tsx
@@ -4,4 +4,39 @@ export const StyledHomePage = styled.div`
   width: 80%;
   height: calc(100vh - 70px);
   margin-top: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+`;
+
+export const StyledCategoriesWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  width: 100%;
+  height: 10%;
+`;
+
+export const StyledLoadmapWrapper = styled.div`
+  width: 100%;
+  height: 90%;
+  background-color: tomato;
+`;
+
+export const StyledCategories = styled.ul`
+  position: absolute;
+  top: 50;
+  left: 50;
+`;
+
+export const StyledSelectButton = styled.input`
+  width: 300px;
+  border: none;
+  border-radius: 15px;
+  padding: 10px;
 `;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,46 @@
-import { StyledHomePage } from "./HomePage.style";
+import { useState } from "react";
+import {
+  StyledCategories,
+  StyledCategoriesWrapper,
+  StyledHomePage,
+  StyledLoadmapWrapper,
+  StyledSelectButton
+} from "./HomePage.style";
 
 export default function HomePage() {
+  const [isToggled, setIsToggled] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('직무를 선택하세요');
+
+  const handleSelectBoxToggle = () => {
+    setIsToggled((currentState) => !currentState);
+  };
+
   return (
     <StyledHomePage>
-      <div>아오..</div>
+      <StyledCategoriesWrapper>
+        <StyledSelectButton
+          type="button"
+          onClick={handleSelectBoxToggle}
+          value={selectedCategory}
+        />
+        {isToggled &&
+          <StyledCategories >
+            <li>
+              <button type="button" >
+                Python
+              </button>
+            </li>
+            <li>
+              <button type="button" >
+                Java
+              </button>
+            </li>
+          </StyledCategories>
+        }
+      </StyledCategoriesWrapper>
+      <StyledLoadmapWrapper>
+        loadmap
+      </StyledLoadmapWrapper>
     </StyledHomePage>
   );
 };
