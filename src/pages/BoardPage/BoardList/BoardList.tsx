e0a44ddd69424bfebf6f6e0b16ac14b9ab13b2de
task11 <task11@naver.com>
2022-10-30 17:04:01 +0900

    feat: BoardList 컴포넌트 구현

---
+++ src/pages/BoardPage/BoardList/BoardList.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+import BoardItem from "../BoardItem/BoardItem";
+
+const boardList = [
+  {
+    id: 1,
+    author: 'task11',
+    summary_profile_img: '',
+    likes: 5,
+    comment_length: 6,
+    views: 100,
+    title: 'Lorem ipsum dolor sit',
+    summary_description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita reiciendis dolorem, cum sit alias tempora amet voluptate maiores aperiam voluptates nisi delectus quas,',
+    tags: [
+      'react', 'html'
+    ]
+  },
+  {
+    id: 2,
+    author: 'task12',
+    summary_profile_img: '',
+    likes: 6,
+    comment_length: 7,
+    views: 130,
+    title: 'Lorem ipsum dolor sit22',
+    summary_description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    tags: [
+      'react', 'html', '2312322'
+    ]
+  },
+  {
+    id: 3,
+    author: 'task1222',
+    summary_profile_img: '',
+    likes: 3,
+    comment_length: 10,
+    views: 200,
+    title: 'Lorem ipsum dolor sit22666666',
+    summary_description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita reiciendis dolorem, cum sit alias tempora amet voluptate maiores ',
+    tags: [
+      'react', 'html', 'css', 'asdasd'
+    ]
+  },
+  {
+    id: 4,
+    author: 'task1222',
+    likes: 3,
+    comment_length: 10,
+    views: 200,
+    title: 'Lorem ipsum dolor sit22666666',
+    summary_description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita reiciendis dolorem, cum sit alias tempora amet voluptate maiores ',
+    tags: [
+      'react', 'html', 'css', 'asdasd', 'a888', '22323'
+    ]
+  }
+];
+
+export default function BoardList() {
+  return (
+    <StyledBoardList>
+      {boardList.map((board) => (
+        <BoardItem board={board} />
+      ))}
+    </StyledBoardList>
+  );
+}
+
+const StyledBoardList = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`;
