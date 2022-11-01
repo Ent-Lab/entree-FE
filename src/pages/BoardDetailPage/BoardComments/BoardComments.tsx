3d4834801a53ecafdeb3b3765508db440c779c7e
task11 <task11@naver.com>
2022-11-01 17:04:19 +0900

    feat: Board comment detail 페이지 작업을 위한 상위 컴포넌트 수정

--- src/pages/BoardDetailPage/BoardComments/BoardComments.tsx
+++ src/pages/BoardDetailPage/BoardComments/BoardComments.tsx
@@ -1,25 +1,41 @@
 import styled from "styled-components";
 
+import BoardCommentDetail from "../BoardCommentDetail/BoardCommentDetail";
+
 const comments = [
   {
     id: 1,
-    username: 'aaa',
+    user: {
+      name: '하잇',
+      avatar: ''
+    },
     description: '리뷰!',
+    createdAt: '2021-10-31'
   },
   {
     id: 2,
-    username: 'aaa@@@@',
+    user: {
+      name: 'task11',
+      avatar: ''
+    },
     description: '리뷰 남깁니다 좋네요',
+    createdAt: '2021-10-30'
   }
 ];
 
 export default function BoardComments() {
   return (
-    <StyledBoardComments>BoardComments</StyledBoardComments>
+    <StyledBoardComments>
+      <BoardCommentDetail comments={comments} />
+    </StyledBoardComments>
   );
 }
 
 const StyledBoardComments = styled.div`
   width: 100%;
   min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  gap: 20px;
 `;
