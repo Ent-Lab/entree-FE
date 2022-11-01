57eab34d39cb6913aa4fe1d2096965d367280689
task11 <task11@naver.com>
2022-11-01 17:08:52 +0900

    refactor: Board commment 관련 컴포넌트 / 스타일 로직 분리

--- src/pages/BoardDetailPage/BoardCommentForm/BoardCommentForm.tsx
+++ src/pages/BoardDetailPage/BoardCommentForm/BoardCommentForm.tsx
@@ -2,7 +2,14 @@ import Avatar from "@/components/Avatar/Avatar";
 
 import { useState } from "react";
 
-import styled from "styled-components";
+import {
+  StyledBoardCommentAvatar,
+  StyledBoardCommentForm,
+  StyledCommentForm,
+  StyledCommentLengthCheck,
+  StyledCommentSubmitButton,
+  StyledCommentTextArea
+} from "./BoardCommentForm.style";
 
 const maxLength = 200;
 
@@ -46,66 +53,3 @@ export default function BoardCommentForm() {
     </StyledBoardCommentForm >
   );
 }
-
-const StyledBoardCommentForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  gap: 10px;
-`;
-
-const StyledBoardCommentAvatar = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 10px;
-`;
-
-const StyledCommentForm = styled.form`
-  width: 100%;
-  height: 150px;
-  position: relative;
-`;
-
-const StyledCommentTextArea = styled.textarea`
-  width: 100%;
-  height: 100%;
-  padding: 15px;
-  color: ${(props) => props.theme.textColor};
-  background-color: ${(props) => props.theme.bgColor};
-  border: 1px solid ${(props) => props.theme.borderBottom};
-  border-radius: 10px;
-  outline-color: ${(props) => props.theme.borderBottom};
-  resize: none;
-  font-size: 1.8rem;
-`;
-
-const StyledCommentLengthCheck = styled.div`
-  position: absolute;
-  left: 20px;
-  bottom: 20px;
-`;
-
-const StyledCommentSubmitButton = styled.button<{ disabled: boolean; }>`
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
-  border: none;
-  padding: 8px 20px;
-  border-radius: 15px;
-  background-color: ${(props) => props.disabled ? 'lightgray' : '#15b1ff'};
-  font-size: 1.6rem;
-  font-weight: 500;
-  transition: all 0.2s ease -in;
-
-  ${(props) => !props.disabled && (
-    `
-      cursor: pointer;
-      &:hover{
-      background-color: #0487c8;
-    }
-    `
-  )}
-  
-`;
--- src/pages/BoardDetailPage/BoardCommentItem/BoardCommentItem.tsx
+++ src/pages/BoardDetailPage/BoardCommentItem/BoardCommentItem.tsx
@@ -1,10 +1,19 @@
-import { useEffect, useRef, useState } from "react";
-
-import styled from "styled-components";
+import { useState } from "react";
 
 import Avatar from "@/components/Avatar/Avatar";
 
-import { AiOutlineEllipsis } from "@react-icons/all-files/ai/AiOutlineEllipsis";
+import {
+  StlyedDropDownItem,
+  StyledCommentContents,
+  StyledCommentDropDown,
+  StyledCommentEllipsis,
+  StyledCommentHeader,
+  StyledCommentUpdateTime,
+  StyledCommentUserInfo,
+  StyledCommentUserName,
+  StyledCommentWrapper,
+  StyledTextInfoWrapper
+} from "./BoardCommentItem.style";
 
 export default function BoardCommentItem({ comment }) {
   const [dropDownState, setDropDownState] = useState(false);
@@ -58,76 +67,3 @@ export default function BoardCommentItem({ comment }) {
     </StyledCommentWrapper>
   );
 }
-
-const StyledCommentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${(props) => props.theme.bgColor};
-  border-radius: 10px;
-  padding: 15px;
-`;
-
-const StyledCommentHeader = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const StyledCommentUserInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-`;
-
-const StyledCommentUserName = styled.span`
-  font-size: 1.8rem;
-  font-weight: 400;
-`;
-
-const StyledCommentUpdateTime = styled.span`
-  font-size: 1.4rem;
-  color: gray;
-`;
-
-const StyledTextInfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledCommentContents = styled.div`
-  padding: 10px;
-`;
-
-const StyledCommentEllipsis = styled(AiOutlineEllipsis)`
-  font-size: 2rem;
-  cursor: pointer;
-`;
-
-const StyledCommentDropDown = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  right: -50px;
-  top: 15px;
-  width: 80px;
-  height: 100px;
-  border: 1px solid ${(props) => props.theme.borderBottom};
-  border-radius: 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
-const StlyedDropDownItem = styled.div`
-  padding: 5px 15px;
-  cursor: pointer;
-  border-radius: 10px;
-
-  &:hover{
-    background-color: ${(props) => props.theme.bgColor};
-    color: ${(props) => props.theme.accentColor};
-  }
-`;
