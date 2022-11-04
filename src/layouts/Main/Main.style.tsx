6fc2f7cfe4f2fc7b631f74eed90e170467234a01
task11 <task11@naver.com>
2022-11-04 17:31:39 +0900

    fix: 레이아웃 컴포넌트 수정

--- src/layouts/Main/Main.style.tsx
+++ src/layouts/Main/Main.style.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledMainContainer = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   flex-direction:column;
   transition: background 0.2s ease-in, color 0.2s ease-in;
 `;
